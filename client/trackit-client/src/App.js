1b49298d7408fc4663d042eae3d37d0629da4fb1
System Administrator <root@Sankalps-MacBook-Pro.local>
2018-01-08 22:41:37 -0800

    client as a react js app

---
+++ client/trackit-client/src/App.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Track It!</h1>
+        </header>
+        <p className="App-intro">
+          Enter URL and XPath below         
+        </p>        
+        <p className="App-input">
+          <input type="text" defaultValue="Enter URL" className="App-input-url"/>          
+        </p>
+        <p>
+          <input type="text" defaultValue="Enter XPath" className="App-input-xpath"/>
+        </p>
+        <p>
+          <input type="button" value="Track!"/>
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
